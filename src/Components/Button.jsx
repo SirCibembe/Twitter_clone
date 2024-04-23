1f995af7454647e1bbc046b40842bc977b4e068d
sir_cibembe <espoircibembe2@gmail.com>
2024-04-23 08:29:13 +0200

    suite sprint

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,8 +1,30 @@
-import  React from "react";
-// const inputText () => {
-   
-// }
 
-export default function Button (){
-    return <button  /*onClick={}*/ class="text-transform: capitalize px-4 py-2 rounded-full bg-[#1D9BF0] text-white text-sm"className="button">Tweet</button>
+import React, { useState } from "react";
+
+export default function Button() {
+    const [buttonSize, setButtonSize] = useState(16);
+    const [isReduced, setIsReduced] = useState(false);
+
+    const handleClick = () => {
+        setTimeout(() => {
+            setButtonSize(buttonSize * 0.9)
+        }, 20)
+        // if (!isReduced) {
+        //     setButtonSize(16);
+        // } else {
+        //     setButtonSize(buttonSize * 0.9);
+        // }
+        // setIsReduced(!isReduced);
+        //setButtonSize(16)
+    };
+
+    return (
+        <button 
+            className="text-transform: capitalize px-4 py-2 rounded-full bg-[#1D9BF0] text-white text-sm" 
+            style={{ fontSize: buttonSize }} 
+            onClick={handleClick} 
+        >
+            Tweet
+        </button>
+    );
 }
